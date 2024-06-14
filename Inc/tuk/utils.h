1e4d90c2358beace3922ac0a3ad912b4f7297fb1
Logan Furedi <furedil@myumanitoba.ca>
2024-06-14 16:57:50 -0500

    Feat: Add utils.h
    Adds some additional utility functions that don't really fit anywhere in
    particular.

---
+++ Inc/tuk/utils.h
@@ -0,0 +1,36 @@
+/**
+ * @file utils.h
+ * Provides useful miscellaneous utilities for all subsystems.
+ *
+ * @author Logan Furedi <logan.furedi@umsats.ca>
+ *
+ * @date Jun 14, 2024
+ */
+
+#ifndef TSAT_UTILITIES_KIT_INC_TUK_UTILS_H_
+#define TSAT_UTILITIES_KIT_INC_TUK_UTILS_H_
+
+#include <stdint.h>
+
+/**
+ * @brief Converts a big-endian (MSB first) buffer of two 8-bit values into a
+ *        16-bit value with the processor's native endianness.
+ */
+static inline uint16_t BE_To_Native_16(const uint8_t *buf)
+{
+	return (uint16_t)((uint16_t)buf[0] << 8 | (uint16_t)buf[1]);
+}
+
+/**
+ * @brief Converts a big-endian (MSB first) buffer of four 8-bit values into a
+ *        32-bit value with the processor's native endianness.
+ */
+static inline uint32_t BE_To_Native_32(const uint8_t *buf)
+{
+	return (uint32_t)buf[0] << 24 |
+	       (uint32_t)buf[1] << 16 |
+	       (uint32_t)buf[2] << 8 |
+	       (uint32_t)buf[3];
+}
+
+#endif /* TSAT_UTILITIES_KIT_INC_TUK_UTILS_H_ */
