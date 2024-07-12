c322760477a722b74b49b8f87da83d42ce1e37b9
Logan Furedi <furedil@myumanitoba.ca>
2024-07-12 17:18:56 -0500

    style: conform to MISRA C 2004 rule 9.3
    Rule 9.3 of MISRA C 2004 states:
    
    In an enumerator list, the "=" construct shall not be used to explicitly
    initialise members other than the first, unless all items are explicitly
    initialised.

--- Inc/tuk/can_wrapper/can_command_list.h
+++ Inc/tuk/can_wrapper/can_command_list.h
@@ -33,81 +33,83 @@ typedef enum
 	/// COMMON
 	////////////////////////////////////////////
 	CMD_PREPRARE_FOR_SHUTDOWN            = 0x00,
-	CMD_RESET,
-	CMD_GET_PCB_TEMP,
-	CMD_GET_MCU_TEMP,
+	CMD_RESET                            = 0x01,
+	CMD_GET_PCB_TEMP                     = 0x02,
+	CMD_GET_MCU_TEMP                     = 0x03,
 
 	////////////////////////////////////////////
 	/// CDH
 	////////////////////////////////////////////
 	// Event Processing.
 	CMD_CDH_PROCESS_HEARTBEAT            = 0x10,
-	CMD_CDH_PROCESS_ERROR,
-	CMD_CDH_PROCESS_READY_FOR_SHUTDOWN,
-	CMD_CDH_PROCESS_STARTUP,
-	CMD_CDH_PROCESS_PCB_TEMP,
-	CMD_CDH_PROCESS_MCU_TEMP,
-	CMD_CDH_PROCESS_CONVERTER_STATUS,
-	CMD_CDH_PROCESS_BATTERY_VOLTAGE,
-	CMD_CDH_PROCESS_WELL_LIGHT,
-	CMD_CDH_PROCESS_WELL_TEMP,
-	CMD_CDH_PROCESS_LED_TEST,
+	CMD_CDH_PROCESS_ERROR                = 0x11,
+	CMD_CDH_PROCESS_READY_FOR_SHUTDOWN   = 0x12,
+	CMD_CDH_PROCESS_STARTUP              = 0x13,
+	CMD_CDH_PROCESS_PCB_TEMP             = 0x14,
+	CMD_CDH_PROCESS_MCU_TEMP             = 0x15,
+	CMD_CDH_PROCESS_CONVERTER_STATUS     = 0x16,
+	CMD_CDH_PROCESS_BATTERY_VOLTAGE      = 0x17,
+	CMD_CDH_PROCESS_MAGNETIC_FIELD       = 0x18,
+	CMD_CDH_PROCESS_ANGULAR_VELOCITY     = 0x19,
+	CMD_CDH_PROCESS_WELL_LIGHT           = 0x1A,
+	CMD_CDH_PROCESS_WELL_TEMP            = 0x1B,
+	CMD_CDH_PROCESS_LED_TEST             = 0x1C,
 
 	// Tests
-	CMD_CDH_TEST_FLASH,
-	CMD_CHD_TEST_MRAM,
+	CMD_CDH_TEST_FLASH                   = 0x1D,
+	CMD_CDH_TEST_MRAM                    = 0x1E,
 
 	// Antenna
-	CMD_CDH_ENABLE_ANTENNA_DEPLOYMENT,
-	CMD_CDH_DEPLOY_ANTENNA,
-	CMD_CDH_TRANSMIT_UHF_BEACON,
+	CMD_CDH_ENABLE_ANTENNA_DEPLOYMENT    = 0x1F,
+	CMD_CDH_DEPLOY_ANTENNA               = 0x20,
+	CMD_CDH_TRANSMIT_UHF_BEACON          = 0x21,
 
 	// RTOS
-	CMD_CDH_GET_NUM_TASKS,
-	CMD_CDH_SCHEDULE_SAMPLE_TASK,
+	CMD_CDH_GET_NUM_TASKS                = 0x22,
+	CMD_CDH_SCHEDULE_SAMPLE_TASK         = 0x23,
 
 	// CLock
-	CMD_CDH_SET_RTC,
-	CMD_CDH_GET_RTC,
+	CMD_CDH_SET_RTC                      = 0x24,
+	CMD_CDH_GET_RTC                      = 0x25,
 
-	CMD_CDH_SET_TELEMETRY_INTERVAL,
+	CMD_CDH_SET_TELEMETRY_INTERVAL       = 0x26,
 
 	////////////////////////////////////////////
 	/// POWER
 	////////////////////////////////////////////
 	CMD_PWR_SET_LINE_POWER               = 0x30,
-	CMD_PWR_SET_BATTERY_HEATER,
-	CMD_PWR_GET_CONVERTER_STATUS,
-	CMD_PWR_SET_TELEMETRY_INTERVAL,
+	CMD_PWR_SET_BATTERY_HEATER           = 0x31,
+	CMD_PWR_GET_CONVERTER_STATUS         = 0x32,
+	CMD_PWR_SET_TELEMETRY_INTERVAL       = 0x33,
 
 	////////////////////////////////////////////
 	/// ADCS
 	////////////////////////////////////////////
-	CMD_ADCS_SET_MAGNETORQUER_POWER       = 0x40,
-	CMD_ADCS_SET_MAGNETORQUER_DIRECTION,
-	CMD_ADCS_GET_MAGNETIC_FIELD,
-	CMD_ADCS_GET_ANGULAR_VELOCITY,
-	CMD_ADCS_SET_TELEMETRY_INTERVAL,
+	CMD_ADCS_SET_MAGNETORQUER_POWER      = 0x40,
+	CMD_ADCS_SET_MAGNETORQUER_DIRECTION  = 0x41,
+	CMD_ADCS_GET_MAGNETIC_FIELD          = 0x42,
+	CMD_ADCS_GET_ANGULAR_VELOCITY        = 0x43,
+	CMD_ADCS_SET_TELEMETRY_INTERVAL      = 0x44,
 
 	////////////////////////////////////////////
 	/// PAYLOAD
 	////////////////////////////////////////////
 	CMD_PLD_SET_WELL_LED                 = 0x50,
-	CMD_PLD_SET_WELL_HEATER,
-	CMD_PLD_SET_WELL_TEMP,
-	CMD_PLD_GET_WELL_TEMP,
-	CMD_PLD_GET_WELL_LIGHT,
-	CMD_PLD_SET_TELEMETRY_INTERVAL,
-	CMD_PLD_TEST_LEDS,
+	CMD_PLD_SET_WELL_HEATER              = 0x51,
+	CMD_PLD_SET_WELL_TEMP                = 0x52,
+	CMD_PLD_GET_WELL_TEMP                = 0x53,
+	CMD_PLD_GET_WELL_LIGHT               = 0x54,
+	CMD_PLD_SET_TELEMETRY_INTERVAL       = 0x55,
+	CMD_PLD_TEST_LEDS                    = 0x56,
 
 	////////////////////////////////////////////
 	/// GROUND STATION
 	////////////////////////////////////////////
-	CMD_GND_VERIFY_FLASH_TEST             = 0x60,
-	CMD_GND_VERIFY_MRAM_TEST,
-	CMD_GDN_VERIFY_CDH_NUM_TASKS,
-	CMD_GND_VERIFY_SAMPLE_TASK,
-	CMD_GND_VERIFY_RTC
+	CMD_GND_VERIFY_FLASH_TEST            = 0x60,
+	CMD_GND_VERIFY_MRAM_TEST             = 0x61,
+	CMD_GDN_VERIFY_CDH_NUM_TASKS         = 0x62,
+	CMD_GND_VERIFY_SAMPLE_TASK           = 0x63,
+	CMD_GND_VERIFY_RTC                   = 0x64,
 } CmdID;
 
 typedef struct
--- Inc/tuk/error_tracker/error_list.h
+++ Inc/tuk/error_tracker/error_list.h
@@ -11,19 +11,43 @@
 
 typedef enum {
 	// COMMON
-#include "errors/common.txt"
+	ERR_OK = 0,                // No Error.
+	ERR_UNKNOWN_COMMAND,       // Unknown command ID in CAN message.
+	ERR_CAN_WRAPPER_INIT,      // Failed to initialise CAN Wrapper Module.
+	ERR_ADC_CALIBRATION_START,
+	ERR_ADC_GET_VALUE,
+	ERR_ADC_POLL,
+	ERR_ADC_START,
+	ERR_ADC_STOP,
+	ERR_CAN_ACTIVATE_NOTIFICATION,
+	ERR_CAN_CONFIG_FILTER,
+	ERR_CAN_START,
+	ERR_FLASH_LOCK,
+	ERR_FLASH_READ_DATA,
+	ERR_FLASH_UNLOCK,
+	ERR_FLASH_WRITE_DATA,
+	ERR_I2C_RECEIVE,            // I2C_Receive returned an error code.
+	ERR_I2C_TRANSMIT,           // I2C_Transmit returned an error code.
 
 	// CDH
-#include "errors/cdh.txt"
 
 	// POWER
-#include "errors/power.txt"
 
 	// ADCS
-#include "errors/adcs.txt"
 
 	// PAYLOAD
-#include "errors/payload.txt"
+	ERR_PLD_INVALID_WELL_ID,
+	ERR_PLD_TCA9539_CLEAR_PINS,
+	ERR_PLD_TCA9539_GET_PIN,
+	ERR_PLD_TCA9539_GET_PORT,
+	ERR_PLD_TCA9539_INIT,
+	ERR_PLD_TCA9539_INVALID_EXPANDER_ID,
+	ERR_PLD_TCA9539_INVALID_EXPANDER_PIN_ID,
+	ERR_PLD_TCA9539_SET_PIN,
+	ERR_PLD_TCA9539_SET_PORT,
+	ERR_PLD_TCA9548_INIT,
+	ERR_PLD_TCA9548_INVALID_CHANNEL,
+	ERR_PLD_TCA9548_SET_CHANNEL,
 } ErrorID;
 
 #endif /* TSAT_UTILITIES_KIT_INC_TUK_ERROR_TRACKER_ERROR_LIST_H_ */
