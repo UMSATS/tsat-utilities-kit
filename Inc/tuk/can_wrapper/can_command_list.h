67bbb71cafa31e11de9c70100c4f6c759d09f00d
ArnavG-it <arnavgupta468@outlook.com>
2024-08-04 18:59:17 -0500

    Remove enum assignments
    Also replaces binary priority values with decimal.

--- Inc/tuk/can_wrapper/can_command_list.h
+++ Inc/tuk/can_wrapper/can_command_list.h
@@ -17,79 +17,79 @@ typedef enum
 	////////////////////////////////////////////
 	/// COMMON
 	////////////////////////////////////////////
-	CMD_COMM_RESET                       = 0x00,
-	CMD_COMM_PREPARE_FOR_SHUTDOWN        = 0x01,
-	CMD_COMM_GET_TELEMETRY               = 0x02,
-	CMD_COMM_SET_TELEMETRY_INTERVAL      = 0x03,
-	CMD_COMM_GET_TELEMETRY_INTERVAL      = 0x04,
-	CMD_COMM_UPDATE_START                = 0x05,
-	CMD_COMM_UPDATE_LOAD                 = 0x06,
-	CMD_COMM_UPDATE_END                  = 0x07,
+	CMD_COMM_RESET,
+	CMD_COMM_PREPARE_FOR_SHUTDOWN,
+	CMD_COMM_GET_TELEMETRY,
+	CMD_COMM_SET_TELEMETRY_INTERVAL,
+	CMD_COMM_GET_TELEMETRY_INTERVAL,
+	CMD_COMM_UPDATE_START,
+	CMD_COMM_UPDATE_LOAD,
+	CMD_COMM_UPDATE_END,
 
 	////////////////////////////////////////////
 	/// CDH
 	////////////////////////////////////////////
 	// Event Processing.
-	CMD_CDH_PROCESS_HEARTBEAT            = 0x10,
-	CMD_CDH_PROCESS_RUNTIME_ERROR        = 0x11,
-	CMD_CDH_PROCESS_COMMAND_ERROR        = 0x12,
-	CMD_CDH_PROCESS_NOTIFICATION         = 0x13,
-	CMD_CDH_PROCESS_TELEMETRY_REPORT     = 0x14,
-	CMD_CDH_PROCESS_RETURN               = 0x15,
-	CMD_CDH_PROCESS_LED_TEST             = 0x16,
+	CMD_CDH_PROCESS_HEARTBEAT,
+	CMD_CDH_PROCESS_RUNTIME_ERROR,
+	CMD_CDH_PROCESS_COMMAND_ERROR,
+	CMD_CDH_PROCESS_NOTIFICATION,
+	CMD_CDH_PROCESS_TELEMETRY_REPORT,
+	CMD_CDH_PROCESS_RETURN,
+	CMD_CDH_PROCESS_LED_TEST,
 
 	// Clock
-	CMD_CDH_SET_RTC                      = 0x17,
-	CMD_CDH_GET_RTC                      = 0x18,
+	CMD_CDH_SET_RTC,
+	CMD_CDH_GET_RTC,
 
 	// Tests
-	CMD_CDH_TEST_FLASH                   = 0x19,
-	CMD_CDH_TEST_MRAM                    = 0x1A,
+	CMD_CDH_TEST_FLASH,
+	CMD_CDH_TEST_MRAM,
 
-	CMD_CDH_RESET_SUBSYSTEM              = 0x1B,
+	CMD_CDH_RESET_SUBSYSTEM,
 
 	// Antenna
-	CMD_CDH_ENABLE_ANTENNA               = 0x1C,
-	CMD_CDH_DEPLOY_ANTENNA               = 0x1D,
+	CMD_CDH_ENABLE_ANTENNA,
+	CMD_CDH_DEPLOY_ANTENNA,
 
 	////////////////////////////////////////////
 	/// POWER
 	////////////////////////////////////////////
-	CMD_PWR_PROCESS_HEARTBEAT            = 0x20,
-	CMD_PWR_SET_SUBSYSTEM_POWER          = 0x21,
-	CMD_PWR_GET_SUBSYSTEM_POWER          = 0x22,
-	CMD_PWR_SET_BATTERY_HEATER_POWER     = 0x23,
-	CMD_PWR_GET_BATTERY_HEATER_POWER     = 0x24,
-	CMD_PWR_SET_BATTERY_ACCESS           = 0x25,
-	CMD_PWR_GET_BATTERY_ACCESS           = 0x26,
+	CMD_PWR_PROCESS_HEARTBEAT,
+	CMD_PWR_SET_SUBSYSTEM_POWER,
+	CMD_PWR_GET_SUBSYSTEM_POWER,
+	CMD_PWR_SET_BATTERY_HEATER_POWER,
+	CMD_PWR_GET_BATTERY_HEATER_POWER,
+	CMD_PWR_SET_BATTERY_ACCESS,
+	CMD_PWR_GET_BATTERY_ACCESS,
 
 	////////////////////////////////////////////
 	/// ADCS
 	////////////////////////////////////////////
-	CMD_ADCS_SET_MAGNETORQUER_DIRECTION  = 0x30,
-	CMD_ADCS_GET_MAGNETORQUER_DIRECTION  = 0x31,
-	CMD_ADCS_SET_OPERATING_MODE          = 0x32,
-	CMD_ADCS_GET_OPERATING_MODE          = 0x33,
+	CMD_ADCS_SET_MAGNETORQUER_DIRECTION,
+	CMD_ADCS_GET_MAGNETORQUER_DIRECTION,
+	CMD_ADCS_SET_OPERATING_MODE,
+	CMD_ADCS_GET_OPERATING_MODE,
 
 	////////////////////////////////////////////
 	/// PAYLOAD
 	////////////////////////////////////////////
-	CMD_PLD_SET_ACTIVE_ENVS              = 0x40,
-	CMD_PLD_GET_ACTIVE_ENVS              = 0x41,
-	CMD_PLD_SET_SETPOINT                 = 0x42,
-	CMD_PLD_GET_SETPOINT                 = 0x43,
-	CMD_PLD_SET_TOLERANCE                = 0x44,
-	CMD_PLD_GET_TOLERANCE                = 0x45,
-	CMD_PLD_TEST_LEDS                    = 0x46,
+	CMD_PLD_SET_ACTIVE_ENVS,
+	CMD_PLD_GET_ACTIVE_ENVS,
+	CMD_PLD_SET_SETPOINT,
+	CMD_PLD_GET_SETPOINT,
+	CMD_PLD_SET_TOLERANCE,
+	CMD_PLD_GET_TOLERANCE,
+	CMD_PLD_TEST_LEDS,
 
 	////////////////////////////////////////////
 	/// GROUND STATION
 	////////////////////////////////////////////
-	CMD_GND_VERIFY_FLASH_TEST            = 0x50,
-	CMD_GND_VERIFY_MRAM_TEST             = 0x51,
-	CMD_GDN_VERIFY_CDH_NUM_TASKS         = 0x52,
-	CMD_GND_VERIFY_SAMPLE_TASK           = 0x53,
-	CMD_GND_VERIFY_RTC                   = 0x54,
+	CMD_GND_VERIFY_FLASH_TEST,
+	CMD_GND_VERIFY_MRAM_TEST,
+	CMD_GDN_VERIFY_CDH_NUM_TASKS,
+	CMD_GND_VERIFY_SAMPLE_TASK,
+	CMD_GND_VERIFY_RTC,
 } CmdID;
 
 typedef struct
